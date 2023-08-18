996ffb49d38e29fce18bec73161f5318112067a3
NTiger07 <falaleru@gmail.com>
2023-08-18 16:16:55 +0100

    added route comments

--- app/posts/new/page.jsx
+++ app/posts/new/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// localhost:3000/posts/new
 const New = () => {
   return <div>CREATE NEW POST</div>;
 };
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// localhost:3000/posts
 const Posts = () => {
   return <div>POSTS</div>;
 };
