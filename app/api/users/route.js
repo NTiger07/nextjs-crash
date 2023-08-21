3bae91fb3c45acae289750a521dd820776f08fe7
NTiger07 <falaleru@gmail.com>
2023-08-21 15:37:28 +0100

    route.js

---
+++ app/api/users/route.js
@@ -0,0 +1,11 @@
+export async function GET(request) {
+    // Handle GET request for api/users
+    // Retrieve users fro the database or any data source
+    const users = [
+        { id: 1, name: "John" },
+        { id: 2, name: "Jane" },
+        { id: 3, name: "Bob" }
+    ]
+
+    return new Response(JSON.stringify(users))
+}
